5f0eabd2a8654faa04e2e9d56532d753d9664229
NEUmmc <bmc.bob@qq.com>
2020-05-27 18:36:25 +0800

    alt f 规范了app.vue的代码格式
    无改动

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,39 @@
 <template>
   <div id="app">
-    <router-view v-if="isRouterAlive"/>
+    <router-view v-if="isRouterAlive" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-    provide () {
+  name: "App",
+  provide() {
     return {
       reload: this.reload
-    }
+    };
   },
-  data () {
+  data() {
     return {
       isRouterAlive: true
-    }
+    };
   },
   methods: {
-    reload () {
-      this.isRouterAlive = false
-      this.$nextTick(function () {
-        this.isRouterAlive = true
-      })
+    reload() {
+      this.isRouterAlive = false;
+      this.$nextTick(function() {
+        this.isRouterAlive = true;
+      });
     }
   }
-}
+};
 </script>
 
 <style>
-body{
+body {
   margin: 0;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
