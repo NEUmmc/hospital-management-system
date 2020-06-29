268e82c0088173f675bebfe4ce3854974bb6f1d7
NEUmmc <bmc.bob@qq.com>
2020-06-29 17:37:26 +0800

    滚动条的调整

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,7 @@ export default {
 <style>
 body {
   margin: 0;
+  overflow-y: hidden;
 }
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
--- src/components/common/Content.vue
+++ src/components/common/Content.vue
@@ -12,6 +12,8 @@ export default {};
 
 <style scoped>
 .app-main-container {
+  overflow: auto;
+  height: calc(100vh - 100px);
   position: relative;
   padding: 20px;
   background-color: rgb(255, 255, 255);
